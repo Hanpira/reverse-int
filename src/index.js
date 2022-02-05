5c18d13cb721a042ceb768f6b823afb0c5414cbb
Hanpiranpm <annromania@gmail.com>
2022-02-05 19:09:40 +0300

    refactor: add better condition

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = function reverse (n) {
     let result = 0;
     let num = Math.abs(n);
-    for (let i = 0; num !=0 ; num = Math.floor(num / 10), i++) {
+    for (let i = 0; num > 0 ; num = Math.floor(num / 10), i++) {
         let digit = num % 10;
         result = result*Math.floor(10,i)+digit;
     }
-    return(result);
+    return result;
 }
